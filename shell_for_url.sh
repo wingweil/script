63ad5a06ed2fbddaef6eea6fc506882affb20ddf
wingweil <42139165+wingweil@users.noreply.github.com>
2019-09-23 16:26:31 +0800

    Update shell_for_url.sh

--- shell_for_url.sh
+++ shell_for_url.sh
@@ -1,8 +1,8 @@
 #shell for url:
 #for instance:
 
-callDatahub(){
-  curl -X POST http://100.69.216.39/datahub/post/taskStatus \
+callWingdata(){
+  curl -X POST http://*.*.*.*/Wingdata/post/taskStatus \
 	--header "Content-Type: application/json" \
 	--header "Accept: application/json" \
 	-d "{
@@ -23,23 +23,23 @@ ftp_port=$src_port
 gate=${p1}
 log_name=${p2}
 ftp_adm=${p3}
-ftp_data_rootdir=/nfsvol/cmcs_ftp/cmcs/$gate/${pdate}
+ftp_data_rootdir=/nfsvol/yun_ftp/yun/$gate/${pdate}
 local_record=$sh_directory/record.log
-info='成员企业未上传文件,请联系相关人员处理.'
+info='未上传文件,请联系相关人员处理.'
 taskName=""
 taskNo=""
 business=""
 date=""
 member=""
 message=""
-cmwt_host="###.####.com"
+sxdm_host="###.####.com"
 # 默认端口
-cmwt_port=8060
+sxdm_port=8060
 
-if [[ $cmwt_host == *cmwt* ]];then
-    url='https://'$cmwt_host'/cmwt/handleInfo'
+if [[ $sxdm_host == *sxdm* ]];then
+    url='https://'$sxdm_host'/sxdm/handleInfo'
 else
-    url='http://'$cmwt_host':'$port'/###/handleInfo'
+    url='http://'$sxdm_host':'$port'/###/handleInfo'
 fi
 callwqe(){
   curl -X POST ${url} \
@@ -64,7 +64,7 @@ cat /dev/null > ${local_record}
 lftp $ftp_user:$ftp_pwd@$ftp_server:$p3 -e "cd ${ftp_data_rootdir};exit" > ${local_record} 2>&1
 return_result=`grep -c 'Failed to change directory' ${local_record}`
   if [[ ${return_result} -eq 0 ]];then
-       echo "成员企业已上传文件！"
+       echo "已上传文件！"
        break
    fi
    print_log 'waiting...'
